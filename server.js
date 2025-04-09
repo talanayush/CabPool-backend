f1fc61bcafc2a0705df88c9a747b6864d290731a
Ayush Talan <ayush.talan07@gmail.com>
2025-04-09 17:26:25 +0530

    cors credentials updated for the testing url

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const ticketRoutes = require("./routes/ticketRoutes");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "https://cab-pool1-frontend.vercel.app", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 
 // Routes
 
